bae9dfeca8ee78c4b6189c7774d42ce2ceb9f321
Danilly Condori <danilly7@gmail.com>
2025-02-05 00:10:45 +0100

    fix: img to public folder

--- frontend/src/components/business/carrousel-img/data/index.ts
+++ frontend/src/components/business/carrousel-img/data/index.ts
@@ -1,16 +1,16 @@
 import { Image } from "../interface";
 
 export const images: Image[] = [
-    { src: 'src/assets/img/exterior.JPG', alt: "Fachada de la Teca de l'Àvia" },
-    { src: 'src/assets/img/interior.JPG', alt: 'Interior de la Teca' },
-    { src: 'src/assets/img/pollo.JPG', alt: "Pollo a l'ast" },
-    { src: 'src/assets/img/bacalao.JPG', alt: 'Bacalao con sanfaina' },
-    { src: 'src/assets/img/fideua.JPG', alt: 'Fideos a la marinera' },
-    { src: 'src/assets/img/alitas.JPG', alt: 'Alitas de pollo rustidas' },
-    { src: 'src/assets/img/caracoles.JPG', alt: 'Caracoles caseros' },
-    { src: 'src/assets/img/escalivada.JPG', alt: 'Escalivada al natural' },
-    { src: 'src/assets/img/escalopa.JPG', alt: 'Escalopa de pollo con patatas y canelones de carne' },
-    { src: 'src/assets/img/esqueixada.JPG', alt: 'Esqueixada de bacalao' },
-    { src: 'src/assets/img/canelones.JPG', alt: 'Canelones de carne' },
-    { src: 'src/assets/img/rusa.JPG', alt: 'Ensaladilla rusa' },
+    { src: 'public/img/exterior.JPG', alt: "Fachada de la Teca de l'Àvia" },
+    { src: 'public/img/interior.JPG', alt: 'Interior de la Teca' },
+    { src: 'public/img/pollo.JPG', alt: "Pollo a l'ast" },
+    { src: 'public/img/bacalao.JPG', alt: 'Bacalao con sanfaina' },
+    { src: 'public/img/fideua.JPG', alt: 'Fideos a la marinera' },
+    { src: 'public/img/alitas.JPG', alt: 'Alitas de pollo rustidas' },
+    { src: 'public/img/caracoles.JPG', alt: 'Caracoles caseros' },
+    { src: 'public/img/escalivada.JPG', alt: 'Escalivada al natural' },
+    { src: 'public/img/escalopa.JPG', alt: 'Escalopa de pollo con patatas y canelones de carne' },
+    { src: 'public/img/esqueixada.JPG', alt: 'Esqueixada de bacalao' },
+    { src: 'public/img/canelones.JPG', alt: 'Canelones de carne' },
+    { src: 'public/img/rusa.JPG', alt: 'Ensaladilla rusa' },
 ];
